95f70be90ac2bd63466d7fe050e73f4efda84039
artiecannv <thefiftharthur@gmail.com>
2023-03-08 16:46:38 -0700

    working on the User model

---
+++ server/config/models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trimmed: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/,
+      "Not a Valid Email Address",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    match: [
+      /^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[^a-zA-Z0-9])(?!.*\s).{8,15}$/,
+      "Not a Valid Password",
+    ],
+  },
+  user_posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user_posts",
+    },
+  ],
+});
