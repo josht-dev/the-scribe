b19b6d71bfb33ff0f228b642dd2c5f3124332120
loudwhisperer <driftaway44@gmail.com>
2023-03-12 12:30:02 -0600

    userpost schema v1

--- server/models/UserPost.js
+++ server/models/UserPost.js
@@ -1,1 +1,58 @@
 const { Schema, model } = require("mongoose");
+const dayjs = require("dayjs");
+const reactionSchema = require("./Reaction");
+
+const userPostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: 8,
+      maxLength: 50,
+    },
+    subject: {
+      type: String,
+      required: true,
+      minLength: 8,
+      maxLength: 50,
+    },
+    body: {
+      type: String,
+      required: true,
+      minLength: 8,
+      maxLength: 3000,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function (date) {
+        return dayjs(date).format("MMM DD YYYY H:m");
+      },
+    },
+    username: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    reactions: [reactionSchema],
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Forum",
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userPostSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const UserPost = model("UserPost", userPostSchema); 
+
+module.exports = UserPost;
