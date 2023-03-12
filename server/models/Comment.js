41c2939849040a2e5d74f6ad2099cbf44a4bea25
artiecannv <thefiftharthur@gmail.com>
2023-03-12 12:22:34 -0600

    updated the comment model and reaction model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,1 +1,35 @@
-const { Schema, model } = require("mongoose");
+const { Schema, Types } = require("mongoose");
+const dayjs = require("dayjs");
+
+const commentSchema = new Schema(
+  {
+    commentId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    commentBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function (date) {
+        return dayjs(date).format("MMM DD YYYY H:m");
+      },
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    _id: false,
+  }
+);
+
+module.exports = commentSchema;
