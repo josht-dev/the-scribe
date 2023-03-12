2d515eae53535d05674162634571f18eb2805e0e
artiecannv <thefiftharthur@gmail.com>
2023-03-12 12:19:39 -0600

    updated the Reaction model

--- server/models/Reaction.js
+++ server/models/Reaction.js
@@ -1,1 +1,35 @@
-const { Schema, model } = require("mongoose");
+const { Schema, Types } = require("mongoose");
+const dayjs = require("dayjs");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function (date) {
+        return dayjs(date).format("MMM DD YYYY H:m");
+      },
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    _id: false,
+  }
+);
+
+module.exports = reactionSchema;
