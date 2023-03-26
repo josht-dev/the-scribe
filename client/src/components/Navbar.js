f03bfbb6d35d6c5c3242b16825bf169419449e88
Joshua Taylor <toasterrage@gmail.com>
2023-03-26 16:12:21 -0600

    Removed extra btn that was not used

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -172,36 +172,6 @@ export default function Navbar() {
               </Button>
             ))}
           </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {notLog.map((notLog) => (
-                <MenuItem key={notLog.id} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{notLog.name}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
         </Toolbar>
       </Container>
     </AppBar>
