e72799647d97f4e48f5420facd88a53c44a056f2
artiecannv <thefiftharthur@gmail.com>
2023-03-25 20:43:57 -0600

    added content for the characterSchema

--- server/models/Character.js
+++ server/models/Character.js
@@ -1,3 +1,34 @@
 const { Schema, model } = require("mongoose");
 
-const characterSchema = new Schema({})
+const characterSchema = new Schema({
+  characterName: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 25,
+  },
+  characterStatus: {
+    type: String,
+    allowNull: true,
+    maxLength: 50,
+  },
+  motivations: [
+    {
+      type: String,
+      allowNull: true,
+    },
+  ],
+  //! TODO: Might be an array, for subdocs
+  characterNotes: {
+    type: String,
+    allowNull: true,
+  },
+  characterSheet: {
+    type: String,
+    allowNull: true,
+  },
+  npc: {
+    type: Boolean,
+    required: true,
+  },
+});
