7f48de4c3cc32cd197e5e291c3503369abc6e5c4
Joshua Taylor <toasterrage@gmail.com>
2023-03-26 19:45:22 -0600

    create the contentModal componenet

---
+++ client/src/components/ContentModal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const styles = {
+  section: {
+    position: 'absolute',
+    top: '9.75rem',
+    width: '74rem',
+    height: '50.75rem',
+    margin: 'auto',
+    backgroundColor: '#fff',
+    borderRadius: '.25rem',
+    boxShadow: '0px 3px 5px -2px rgba(0, 0, 0, 0.2), 0px 2px 3px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)'
+  }
+}
+
+export default function ContentModal() {
+  return (
+    <section style={styles.section}>
+
+    </section>
+  );
+}
