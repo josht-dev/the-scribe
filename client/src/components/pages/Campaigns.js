9a231f29c4aeecf45f4755e3b31320220a5ac769
Joshua Taylor <toasterrage@gmail.com>
2023-03-26 21:09:12 -0600

    Added testing data array of objects

--- client/src/components/pages/Campaigns.js
+++ client/src/components/pages/Campaigns.js
@@ -1,6 +1,72 @@
 import React from "react";
 import ContentModal from '../ContentModal';
 
+
+// Testing data
+const campaignArray = [
+  {
+    _id: 1,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+  {
+    _id: 2,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+  {
+    _id: 3,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+  {
+    _id: 4,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+  {
+    _id: 5,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+  {
+    _id: 6,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+  {
+    _id: 7,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+  {
+    _id: 8,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+  {
+    _id: 9,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+  {
+    _id: 10,
+    title: 'title',
+    game: 'dnd',
+    modifiedAt: '2023-01-08 16:42:33'
+  },
+];
+
+
 export default function Campaigns() {
   return (
     <ContentModal />
