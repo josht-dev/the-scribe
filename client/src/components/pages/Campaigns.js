66b63c247e9539efd9a87c332b253097381262e8
Joshua Taylor <toasterrage@gmail.com>
2023-03-27 13:30:12 -0600

    Got list tabs styled

--- client/src/components/pages/Campaigns.js
+++ client/src/components/pages/Campaigns.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 // Testing data
 const campaignArray = [
   {
@@ -79,22 +78,44 @@ const styles = {
     height: '50rem',
     backgroundColor: '#fff',
     borderRadius: '.25rem',
-    boxShadow: '0px 3px 5px -2px rgba(0, 0, 0, 0.2), 0px 2px 3px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)',
-    // TODO - Remove temporary border styling
-    border: '1px solid black'
+    boxShadow: '0px 3px 5px -2px rgba(0, 0, 0, 0.2), 0px 2px 3px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)'
+  },
+  titleDiv: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '75rem',
   },
   titleBtn: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    margin: '0.5rem',
     padding: '.5rem 1.5rem',
     float: 'left',
-    margin: '0.5rem',
     backgroundColor: '#1CB9B3',
     boxShadow: '0px 3px 5px -2px rgba(0, 0, 0, 0.2), 0px 2px 3px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)',
     borderRadius: '0.25rem',
-    textTransform: 'uppercase',
     color: '#fff'
+  },
+  tabContainer: {
+    margin: '0.5rem 0.5rem 0 0.5rem',
+    display: 'flex'
+  },
+  tab: {
+    padding: '0.5rem 1rem',
+    marginRight: '0.25rem',
+    borderWidth: '2px 2px 0px 2px',
+    borderStyle: 'solid',
+    borderColor: '#1CB9B3',
+    borderRadius: '0.25rem 0.25rem 0 0',
+    fontWeight: 500
+  },
+  listDivLarge: {
+    border: '1px solid #1CB9B3',
+    borderRadius: '0 0.25rem 0.25rem 0.25rem',
+    margin: '0 0.5rem 0.5rem 0.5rem',
+    height: '42.25rem',
+    backgroundColor: '#F5F5F5'
+  },
+  listCardLarge: {
+    backgroundColor: '#fff'
   }
 };
 
@@ -104,11 +125,18 @@ const campaignData = () => {
   
 
   return (
-    <div style={styles.titleBtn}>
-      campaigns
-    </div>
-    
+    <>
+      <div style={styles.titleDiv}>
+        <span style={styles.titleBtn}>campaigns</span>
+      </div>
+      <div style={styles.tabContainer} id='tabContainer'>
+        <span style={styles.tab} className='selectedTab'>your campaigns</span>
+        <span style={styles.tab} className=''>fist full of credits</span>
+      </div>
+      <div style={styles.listDivLarge}>
 
+      </div>
+    </>
   );
 }
 
