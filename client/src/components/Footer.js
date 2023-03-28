931903dc3d661e923c5753784ba866f2d81caf5f
Christin Carter <raspberry.muppet@gmail.com>
2023-03-27 20:06:52 -0600

    styling troubleshooting

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,24 +1,50 @@
 import React from "react";
 import ContactIcon from "../assets/icons/contact-icon.svg";
 import DevsIcon from "../assets/icons/devs-icon.svg";
-import DonateIcon from "../assets/icons/donate-icon.svg"
+import DonateIcon from "../assets/icons/donate-icon.svg";
+import { AppBar, Container, Toolbar } from "@mui/material";
 
 const styles = {
     footer: {
         position: "absolute",
+        width: "100%",
+        margin: "0.5rem",
         bottom: "1rem",
-    }
+        display: "flex",  
+        border: "1px solid black",
+    },
+    leftDiv: {
+        float: "left",
+        border: "1px solid red",
+        width: "40%",
+    },
+    rightDiv: {
+        border: "1px solid blue",
+        width: "60%",
+    },
+    rightImg: {
+        float: "right",
+    },
 }
 
 export default function Footer() {
     return (
-        <section style={styles.footer}>
-            <div>
+        <AppBar position="static" sx={{ 
+            backgroundColor: "transparent",
+            bottom: "1rem",
+            position: "absolute",
+        }} component='footer'>
+        <Container disableGutters maxWidth="xl">
+            <Toolbar disableGutters>
+            <div style={styles.leftDiv}>
                 <img src={ContactIcon} alt="contact icon"></img>
+                <img src={DevsIcon} alt="devs icon"></img>
             </div>
-            <div>
-                <img ></img>
+            <div style={styles.rightDiv}>
+                <img style={styles.rightImg} src={DonateIcon} alt="donate icon"></img>
             </div>
-        </section>
+            </Toolbar>
+         </Container>
+        </AppBar>
     );
 }
