1aaeab589482239a4a8fa6a6381b1e9e28564907
Christin Carter <raspberry.muppet@gmail.com>
2023-03-27 19:17:20 -0600

    imported other icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import ContactIcon from "../assets/icons/contact-icon.svg";
+import DevsIcon from "../assets/icons/devs-icon.svg";
+import DonateIcon from "../assets/icons/donate-icon.svg"
 
-
+const styles = {
+    footer: {
+        position: "absolute",
+        bottom: "1rem",
+    }
+}
 
 export default function Footer() {
-    
+    return (
+        <section style={styles.footer}>
+            <div>
+                <img src={ContactIcon} alt="contact icon"></img>
+            </div>
+            <div>
+                <img ></img>
+            </div>
+        </section>
+    );
 }
