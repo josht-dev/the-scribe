f13b9a4a4dad6821b51200b3183a6a2ee153fc01
Joshua Taylor <toasterrage@gmail.com>
2023-03-29 11:33:51 -0600

    Updated for styling

--- client/src/components/Campaigns/SingleCampaign.js
+++ client/src/components/Campaigns/SingleCampaign.js
@@ -4,27 +4,35 @@ import TitleLarge from './TitleLarge';
 export default function SingleCampaign(props) {
 
   const styles = {
-    divLarge: {
+    section: {
       border: '1px solid #1CB9B3',
       borderRadius: '0 0.25rem 0.25rem 0.25rem',
       margin: '0 0.5rem 0.5rem 0.5rem',
-      height: '42.25rem'
+      height: '42.25rem',
+      padding: '0.5rem'
+    },
+    titleBar: {
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'space-between',
+      border: '1px solid black'
     }
   }
   console.log(props.campaign);
 
   return (
-    <div style={styles.divLarge}>
-      <TitleLarge 
-        placeholder='campaign title'
-        title={props.title}
-      />
-      <TitleLarge 
-        placeholder='game system'
-        title={props.game}
-      />
+    <section style={styles.section}>
+      <div style={styles.titleBar}>
+        <TitleLarge
+          placeholder='campaign title'
+          title={props.title}
+        />
+        <TitleLarge
+          placeholder='game system'
+          title={props.game}
+        />
+      </div>
 
-
-    </div>
+    </section>
   );
 }
