2d57a665f64f2b89afefff25996229bac9d72194
Joshua Taylor <toasterrage@gmail.com>
2023-03-28 21:35:36 -0600

    Created the component for a single campaign

---
+++ client/src/components/Campaigns/SingleCampaign.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function SingleCampaign(props) {
+
+  const styles = {
+    divLarge: {
+      border: '1px solid #1CB9B3',
+      borderRadius: '0 0.25rem 0.25rem 0.25rem',
+      margin: '0 0.5rem 0.5rem 0.5rem',
+      height: '42.25rem'
+    }
+  }
+
+  return (
+    <div style={styles.divLarge}>
+
+
+
+    </div>
+  );
+}
