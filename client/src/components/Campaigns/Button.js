761a982796fac6e51de03b4dd8598a22a65bbc44
Joshua Taylor <toasterrage@gmail.com>
2023-03-29 13:35:50 -0600

    Updated to work better with other components

--- client/src/components/Campaigns/Button.js
+++ client/src/components/Campaigns/Button.js
@@ -2,15 +2,9 @@ import React from 'react';
 
 // Styling object
 const styles = {
-  titleDiv: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '75rem',
-  },
   titleBtn: {
     margin: '0.5rem',
-    padding: '.5rem 1.5rem',
+    padding: '0.5rem 1rem',
     backgroundColor: '#1CB9B3',
     boxShadow: '0px 3px 5px -2px rgba(0, 0, 0, 0.2), 0px 2px 3px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)',
     borderRadius: '0.25rem',
@@ -20,10 +14,8 @@ const styles = {
 
 export default function Button(props) {
   return (
-    <div style={styles.titleDiv}>
       <span 
         style={styles.titleBtn}
       >{props.title}</span>
-    </div>
   );
 }
