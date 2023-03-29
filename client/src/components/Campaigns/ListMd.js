2b956c28de53f006d0586014c87485bdcd7a0675
Joshua Taylor <toasterrage@gmail.com>
2023-03-29 12:57:06 -0600

    Added medium list component

---
+++ client/src/components/Campaigns/ListMd.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const styles = {
+  titleBar: {
+    width: '100%'
+  },
+}
+
+export default function ListMd(props) {
+
+  return (
+    <section>
+      <div style={styles.titleBar}>
+
+      </div>
+
+
+
+    </section>
+  );
+}
