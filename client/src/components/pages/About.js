0d5b16aa1826d487b9f8152c277ac6a7d1a3769f
artiecannv <thefiftharthur@gmail.com>
2023-03-31 18:52:49 -0600

    added grid to dev names with vertical divider between each

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,20 +5,37 @@ const About = () => {
   return (
     <main>
       {/* Contributors */}
-      <div>
-        <Link href="https://github.com/artiecannv" underline="hover">
-          ARTIE
-        </Link>
-        <Link href="https://github.com/loudwhisperer" underline="hover">
-          BRENDAN
-        </Link>
-        <Link href="https://github.com/porkchoppy" underline="hover">
-          CHRISTIN
-        </Link>
-        <Link href="https://github.com/josht-dev" underline="hover">
-          JOSH
-        </Link>
-      </div>
+      <Grid container>
+        <Grid item>
+          <Link href="https://github.com/artiecannv" underline="hover">
+            ARTIE
+          </Link>
+        </Grid>
+        <Divider orientation="vertical" flexItem>
+          VERTICAL
+        </Divider>
+        <Grid item>
+          <Link href="https://github.com/loudwhisperer" underline="hover">
+            BRENDAN
+          </Link>
+        </Grid>
+        <Divider orientation="vertical" flexItem>
+          VERTICAL
+        </Divider>
+        <Grid item>
+          <Link href="https://github.com/porkchoppy" underline="hover">
+            CHRISTIN
+          </Link>
+        </Grid>
+        <Divider orientation="vertical" flexItem>
+          VERTICAL
+        </Divider>
+        <Grid item>
+          <Link href="https://github.com/josht-dev" underline="hover">
+            JOSH
+          </Link>
+        </Grid>
+      </Grid>
     </main>
   );
 };
