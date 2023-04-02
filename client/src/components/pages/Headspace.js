3b04d40519ac2dc962b1ba3cd4ef759c45766e69
artiecannv <thefiftharthur@gmail.com>
2023-04-02 16:19:38 -0600

    adding seeds for testing purposes

--- client/src/components/pages/Headspace.js
+++ client/src/components/pages/Headspace.js
@@ -1,3 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+
+const headspaceArray = [
+    // Empty Array Item for Seeds
+    //! CreatedAt?
+    {
+        _id: "0",
+        title: "",
+        subject: "",
+        body: "",
+        username: "",
+        comments: [
+            {
+                _id: "",
+                commentBody: "",
+                commentWriter: "",
+            }
+        ],
+        category: "",
+    },
+    {
+        _id: "1",
+        title: "Can I get some help with loot distribution?",
+        subject: "",
+        body: "",
+        username: "",
+        comments: [
+            {
+                _id: "",
+                commentBody: "",
+                commentWriter: "",
+            }
+        ],
+        category: "",
+    },
+    {
+        _id: "2",
+        title: "",
+        subject: "",
+        body: "",
+        username: "",
+        comments: [
+            {
+                _id: "",
+                commentBody: "",
+                commentWriter: "",
+            }
+        ],
+        category: "",
+    },
+    {
+        _id: "3",
+        title: "",
+        subject: "",
+        body: "",
+        username: "",
+        comments: [
+            {
+                _id: "",
+                commentBody: "",
+                commentWriter: "",
+            }
+        ],
+        category: "",
+    },
+    {
+        _id: "4",
+        title: "",
+        subject: "",
+        body: "",
+        username: "",
+        comments: [
+            {
+                _id: "",
+                commentBody: "",
+                commentWriter: "",
+            }
+        ],
+        category: "",
+    },
+    {
+        _id: "5",
+        title: "",
+        subject: "",
+        body: "",
+        username: "",
+        comments: [
+            {
+                _id: "",
+                commentBody: "",
+                commentWriter: "",
+            }
+        ],
+        category: "",
+    },
+]
 
 export default function Headspace() {}
