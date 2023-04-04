ab69ccc9d63cc2fd7966a35d074a596516876a6d
loudwhisperer <driftaway44@gmail.com>
2023-04-04 17:45:31 -0600

    user posts show now 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/the-scribe",
-  //"mongodb://localhost/the-scribe",
+  process.env.MONGODB_URI || "mongodb://localhost/the-scribe",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
