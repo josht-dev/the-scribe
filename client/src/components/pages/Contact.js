556216743444d11f71b4b979fda0868a097d7108
Christin Carter <raspberry.muppet@gmail.com>
2023-04-03 20:19:10 -0600

    added submit button

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -3,13 +3,13 @@ import React from "react";
 function Contact() {
     return (
         <div>
-            
             <input type="name" placeholder="Name" id="name">
             </input>
             <input type="email" placeholder="Enter your email" id="email">
             </input>
             <input type="message" id="message">
             </input>
+            <button>Submit</button>
         </div>
     )
 }
