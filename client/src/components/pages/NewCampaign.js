e3596289add222499923037afb97a66b168c2342
Joshua Taylor <toasterrage@gmail.com>
2023-04-05 12:43:30 -0600

    Created new campaign component

---
+++ client/src/components/pages/NewCampaign.js
@@ -0,0 +1,21 @@
+import React, { useState, useRef, Suspense } from "react";
+import Tab from '../Campaigns/Tab';
+import CampaignList from '../Campaigns/CampaignList';
+import SingleCampaign from "../Campaigns/SingleCampaign";
+import Button from '../Campaigns/Button';
+import { gql, useQuery } from "@apollo/client";
+
+
+function Campaign() {
+
+
+
+
+
+
+
+
+  
+}
+
+export default Campaign;
